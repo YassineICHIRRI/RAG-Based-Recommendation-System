fb4b5ef14896da2b9628654d9b4992dc5f01f551
jamormoussa <moussajamorsup@gmail.com>
2024-11-20 02:02:58 +0100

    add setup file for this project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        ""
+    ],
+    # Metadata
+    author='',
+    author_email='',
+    description="",
+    url='',
+)
